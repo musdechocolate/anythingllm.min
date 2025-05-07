fb5fd006fec1189adf3757186808f1138996402f
musdechocolate <mustafa.samir.khalil@gmail.com>
2025-05-07 23:32:39 +0200

    initial commit

---
+++ frontend/src/components/DataConnectorOption/media/index.js
@@ -0,0 +1,17 @@
+import GitHub from "./github.svg";
+import GitLab from "./gitlab.svg";
+import YouTube from "./youtube.svg";
+import Link from "./link.svg";
+import Confluence from "./confluence.jpeg";
+import DrupalWiki from "./drupalwiki.jpg";
+
+const ConnectorImages = {
+  github: GitHub,
+  gitlab: GitLab,
+  youtube: YouTube,
+  websiteDepth: Link,
+  confluence: Confluence,
+  drupalwiki: DrupalWiki,
+};
+
+export default ConnectorImages;
